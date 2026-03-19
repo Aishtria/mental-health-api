4c6a30dda810ef4147dc88e4777134f6b5ed788d
Trishia-Soriano <aishtriasoriano@gmail.com>
2026-03-19 15:18:52 +0800

    Update frontend API URLs to use deployed backend

--- routes/moods.js
+++ routes/moods.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { getAIResponse } from "../aiService.js";
+import { getAIResponse } from "../services/aiService.js";
 import { db } from "../db.js";
 
 const router = express.Router();
